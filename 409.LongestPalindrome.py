67d5d080ab5cf8cf7f0e02ddb94c38fef267de80
Ingred <fonsecaingred@gmail.com>
2023-04-01 13:01:20 -0300

    resolved

--- 409.LongestPalindrome.py
+++ 409.LongestPalindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         frequency = {}
         len_ = 0
+        odd = False
 
         for letter in s:
             frequency[letter] = frequency.get(letter, 0) + 1
@@ -11,9 +12,9 @@ class Solution:
                 len_ += i
             else:
                 len_ += i - 1
-                oddCount = True
+                odd = True
         
-        if oddCount:
+        if odd:
             len_ += 1
         
         return len_
